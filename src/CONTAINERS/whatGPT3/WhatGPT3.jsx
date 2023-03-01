aa3611fe0f9b424102efddf9e2bf886d3de874c2
ArzilRiyaj <64822047+ArzilRiyaj@users.noreply.github.com>
2023-03-02 01:58:32 +0530

    latest

---
+++ src/CONTAINERS/whatGPT3/WhatGPT3.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './whatGPT3.css';
+const whatGPT = () => {
+  return (
+    <div>whatGPT</div>
+  )
+}
+
+export default whatGPT
---
+++ src/components/cta/CTA.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './CTA.css';
+const cta = () => {
+  return (
+    <div>cta</div>
+  )
+}
+
+export default cta
